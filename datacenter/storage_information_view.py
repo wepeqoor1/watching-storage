853ed77486ecd9634dad4a8fb6c599d3dbf4dc5d
wepeqoor1 <hoopeeeee@gmail.com>
2022-09-04 19:25:08 +0300

    Show who is entered in storrage

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,16 +4,15 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
+    visit = Visit.objects.filter(leaved_at=None)
     non_closed_visits = [
         {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
+            'who_entered': person_visit.passcard.owner_name,
+            'entered_at': person_visit.entered_at,
+            'duration': person_visit.get_duration,
+        } for person_visit in visit
     ]
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
