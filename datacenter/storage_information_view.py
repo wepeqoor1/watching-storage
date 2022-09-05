3e60c4fda2d4d922ae83d082473fc94e7b2dc401
wepeqoor1 <hoopeeeee@gmail.com>
2022-09-05 22:54:49 +0300

    Correction ORM request

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,15 +1,14 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Visit, format_values
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    visit = Visit.objects.filter(leaved_at=None)
+    visit = Visit.objects.filter(leaved_at__isnull=True)
     non_closed_visits = [
         {
             'who_entered': person_visit.passcard.owner_name,
             'entered_at': person_visit.entered_at,
-            'duration': person_visit.get_duration,
+            'duration': format_values(person_visit.get_duration),
         } for person_visit in visit
     ]
     context = {
