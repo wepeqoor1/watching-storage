de02cd228c199bf185eeb1b4990f39ced6d8740f
wepeqoor1 <hoopeeeee@gmail.com>
2022-09-06 11:56:11 +0300

    Delete poetry env

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 
 from django.core.management import execute_from_command_line
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+execute_from_command_line('manage.py runserver 127.0.0.1:8000'.split())
+
 from dotenv import load_dotenv
 
-
 load_dotenv()
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split())
